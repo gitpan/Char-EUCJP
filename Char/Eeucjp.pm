c8e67b6978ad874684328aad8f024d126b679a20
INABA Hitoshi <ina@cpan.org>
2013-07-18 13:51:09 +0000

    Import of INA/Char-EUCJP-0.92 from CPAN.
    gitpan-cpan-distribution: Char-EUCJP
    gitpan-cpan-version:      0.92
    gitpan-cpan-path:         INA/Char/EUCJP/Char-EUCJP-0.92.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Char/Eeucjp.pm
+++ Char/Eeucjp.pm
@@ -11,6 +11,14 @@ package Char::Eeucjp;
 use 5.00503;    # Galapagos Consensus 1998 for primetools
 # use 5.008001; # Lancaster Consensus 2013 for toolchains
 
+# 12.3. Delaying use Until Runtime
+# in Chapter 12. Packages, Libraries, and Modules
+# of ISBN 0-596-00313-7 Perl Cookbook, 2nd Edition.
+# (and so on)
+
+BEGIN { eval q{ use vars qw($VERSION) } }
+$VERSION = sprintf '%d.%02d', q$Revision: 0.92 $ =~ /(\d+)/xmsg;
+
 BEGIN {
     if ($^X =~ / jperl /oxmsi) {
         die __FILE__, ": needs perl(not jperl) 5.00503 or later. (\$^X==$^X)";
@@ -22,14 +30,6 @@ BEGIN {
         die __FILE__, ": is not US-ASCII script (must be US-ASCII script).";
     }
 }
-
-# 12.3. Delaying use Until Runtime
-# in Chapter 12. Packages, Libraries, and Modules
-# of ISBN 0-596-00313-7 Perl Cookbook, 2nd Edition.
-# (and so on)
-
-BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.91 $ =~ /(\d+)/xmsg;
 
 BEGIN {
 
@@ -1501,14 +1501,14 @@ sub _charlist_tr {
 #
 sub _cc {
     if (scalar(@_) == 0) {
-        die __FILE__, ": function cc got no parameter.";
+        die __FILE__, ": subroutine cc got no parameter.";
     }
     elsif (scalar(@_) == 1) {
         return sprintf('\x%02X',$_[0]);
     }
     elsif (scalar(@_) == 2) {
         if ($_[0] > $_[1]) {
-            die __FILE__, ": function cc got \$_[0] > \$_[1] parameters).";
+            die __FILE__, ": subroutine cc got \$_[0] > \$_[1] parameters).";
         }
         elsif ($_[0] == $_[1]) {
             return sprintf('\x%02X',$_[0]);
@@ -1521,7 +1521,7 @@ sub _cc {
         }
     }
     else {
-        die __FILE__, ": function cc got 3 or more parameters (@{[scalar(@_)]} parameters).";
+        die __FILE__, ": subroutine cc got 3 or more parameters (@{[scalar(@_)]} parameters).";
     }
 }
 
@@ -1716,7 +1716,7 @@ sub _octets {
         }
     }
     else {
-        die __FILE__, ": function _octets got invalid length ($length).";
+        die __FILE__, ": subroutine _octets got invalid length ($length).";
     }
 }
 
@@ -1800,7 +1800,7 @@ sub _range_regexp {
         }
 
         else {
-            die __FILE__, ": function _range_regexp panic.";
+            die __FILE__, ": subroutine _range_regexp panic.";
         }
     }
 
@@ -2013,7 +2013,7 @@ sub _charlist {
                 }
 
                 else {
-                    die __FILE__, ": function make_regexp panic.";
+                    die __FILE__, ": subroutine make_regexp panic.";
                 }
 
                 if ($length == 1) {
@@ -3223,7 +3223,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   @split = Char::Eeucjp::split();
   @split = Char::Eeucjp::split;
 
-  This function scans a string given by $string for separators, and splits the
+  This subroutine scans a string given by $string for separators, and splits the
   string into a list of substring, returning the resulting list value in list
   context or the count of substring in scalar context. Scalar context also causes
   split to write its result to @_, but this usage is deprecated. The separators
@@ -3236,13 +3236,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   expression modifiers to the /pattern/, like /pattern/i, /pattern/x, etc. The
   //m modifier is assumed when you split on the pattern /^/.
 
-  If $limit is specified and positive, the function splits into no more than that
+  If $limit is specified and positive, the subroutine splits into no more than that
   many fields (though it may split into fewer if it runs out of separators). If
   $limit is negative, it is treated as if an arbitrarily large $limit has been
   specified If $limit is omitted or zero, trailing null fields are stripped from
   the result (which potential users of pop would do wel to remember). If $string
-  is omitted, the function splits the $_ string. If /pattern/ is also omitted or
-  is the literal space, " ", the function split on whitespace, /\s+/, after
+  is omitted, the subroutine splits the $_ string. If /pattern/ is also omitted or
+  is the literal space, " ", the subroutine split on whitespace, /\s+/, after
   skipping any leading whitespace.
 
   A /pattern/ of /^/ is secretly treated if it it were /^/m, since it isn't much
@@ -3310,7 +3310,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   that vary at runtime. As with ordinary patterns, to do run-time compilation only
   once, use /$variable/o.
 
-  As a special case, if the expression is a single space (" "), the function
+  As a special case, if the expression is a single space (" "), the subroutine
   splits on whitespace just as Char::Eeucjp::split with no arguments does. Thus,
   Char::Eeucjp::split(" ") can be used to emulate awk's default behavior. In contrast,
   Char::Eeucjp::split(/ /) will give you as many null initial fields as there are
@@ -3370,7 +3370,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   which is like the y/// operator in the Unix sed program, only better, in
   everybody's humble opinion.
 
-  This function scans a EUC-JP string character by character and replaces all
+  This subroutine scans a EUC-JP string character by character and replaces all
   occurrences of the characters found in $searchlist with the corresponding character
   in $replacementlist. It returns the number of characters replaced or deleted.
   If no EUC-JP string is specified via =~ operator, the $_ variable is translated.
@@ -3395,12 +3395,12 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $chop = Char::Eeucjp::chop();
   $chop = Char::Eeucjp::chop;
 
-  This function chops off the last character of a string variable and returns the
-  character chopped. The Char::Eeucjp::chop function is used primary to remove the newline
+  This subroutine chops off the last character of a string variable and returns the
+  character chopped. The Char::Eeucjp::chop subroutine is used primary to remove the newline
   from the end of an input recoed, and it is more efficient than using a
   substitution. If that's all you're doing, then it would be safer to use chomp,
   since Char::Eeucjp::chop always shortens the string no matter what's there, and chomp
-  is more selective. If no argument is given, the function chops the $_ variable.
+  is more selective. If no argument is given, the subroutine chops the $_ variable.
 
   You cannot Char::Eeucjp::chop a literal, only a variable. If you Char::Eeucjp::chop a list of
   variables, each string in the list is chopped:
@@ -3452,10 +3452,10 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $byte_pos = Char::Eeucjp::index($string,$substr,$byte_offset);
   $byte_pos = Char::Eeucjp::index($string,$substr);
 
-  This function searches for one string within another. It returns the byte position
+  This subroutine searches for one string within another. It returns the byte position
   of the first occurrence of $substring in $string. The $byte_offset, if specified,
   says how many bytes from the start to skip before beginning to look. Positions are
-  based at 0. If the substring is not found, the function returns one less than the
+  based at 0. If the substring is not found, the subroutine returns one less than the
   base, ordinarily -1. To work your way through a string, you might say:
 
   $byte_pos = -1;
@@ -3469,9 +3469,9 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $byte_pos = Char::Eeucjp::rindex($string,$substr,$byte_offset);
   $byte_pos = Char::Eeucjp::rindex($string,$substr);
 
-  This function works just like Char::Eeucjp::index except that it returns the byte
+  This subroutine works just like Char::Eeucjp::index except that it returns the byte
   position of the last occurrence of $substring in $string (a reverse Char::Eeucjp::index).
-  The function returns -1 if $substring is not found. $byte_offset, if specified,
+  The subroutine returns -1 if $substring is not found. $byte_offset, if specified,
   is the rightmost byte position that may be returned. To work your way through a
   string backward, say:
 
@@ -3486,11 +3486,11 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $lc = Char::Eeucjp::lc($string);
   $lc = Char::Eeucjp::lc_;
 
-  This function returns a lowercased version of EUC-JP $string (or $_, if
-  $string is omitted). This is the internal function implementing the \L escape
+  This subroutine returns a lowercased version of EUC-JP $string (or $_, if
+  $string is omitted). This is the internal subroutine implementing the \L escape
   in double-quoted strings.
 
-  You can use the Char::Eeucjp::fc function for case-insensitive comparisons via Char::EUCJP
+  You can use the Char::Eeucjp::fc subroutine for case-insensitive comparisons via Char::EUCJP
   software.
 
 =item Lower case first character of string
@@ -3498,8 +3498,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $lcfirst = Char::Eeucjp::lcfirst($string);
   $lcfirst = Char::Eeucjp::lcfirst_;
 
-  This function returns a version of EUC-JP $string with the first character
-  lowercased (or $_, if $string is omitted). This is the internal function
+  This subroutine returns a version of EUC-JP $string with the first character
+  lowercased (or $_, if $string is omitted). This is the internal subroutine
   implementing the \l escape in double-quoted strings.
 
 =item Upper case string
@@ -3507,11 +3507,11 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $uc = Char::Eeucjp::uc($string);
   $uc = Char::Eeucjp::uc_;
 
-  This function returns an uppercased version of EUC-JP $string (or $_, if
-  $string is omitted). This is the internal function implementing the \U escape
+  This subroutine returns an uppercased version of EUC-JP $string (or $_, if
+  $string is omitted). This is the internal subroutine implementing the \U escape
   in interpolated strings. For titlecase, use Char::Eeucjp::ucfirst instead.
 
-  You can use the Char::Eeucjp::fc function for case-insensitive comparisons via Char::EUCJP
+  You can use the Char::Eeucjp::fc subroutine for case-insensitive comparisons via Char::EUCJP
   software.
 
 =item Upper case first character of string
@@ -3519,13 +3519,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $ucfirst = Char::Eeucjp::ucfirst($string);
   $ucfirst = Char::Eeucjp::ucfirst_;
 
-  This function returns a version of EUC-JP $string with the first character
+  This subroutine returns a version of EUC-JP $string with the first character
   titlecased and other characters left alone (or $_, if $string is omitted).
   Titlecase is "Camel" for an initial capital that has (or expects to have)
   lowercase characters following it, not uppercase ones. Exsamples are the first
   letter of a sentence, of a person's name, of a newspaper headline, or of most
   words in a title. Characters with no titlecase mapping return the uppercase
-  mapping instead. This is the internal function implementing the \u escape in
+  mapping instead. This is the internal subroutine implementing the \u escape in
   double-quoted strings.
 
   To capitalize a string by mapping its first character to titlecase and the rest
@@ -3562,8 +3562,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   $fc = Char::Eeucjp::fc($string);
   $fc = Char::Eeucjp::fc_;
 
-  New to Char::EUCJP software, this function returns the full Unicode-like casefold of
-  EUC-JP $string (or $_, if omitted). This is the internal function implementing
+  New to Char::EUCJP software, this subroutine returns the full Unicode-like casefold of
+  EUC-JP $string (or $_, if omitted). This is the internal subroutine implementing
   the \F escape in double-quoted strings.
 
   Just as title-case is based on uppercase but different, foldcase is based on
@@ -3591,20 +3591,20 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
   @ignorecase = Char::Eeucjp::ignorecase(@string);
 
-  This function is internal use to m/ /i, s/ / /i, split / /i and qr/ /i.
+  This subroutine is internal use to m/ /i, s/ / /i, split / /i and qr/ /i.
 
 =item Make capture number
 
   $capturenumber = Char::Eeucjp::capture($string);
 
-  This function is internal use to m/ /, s/ / /, split / / and qr/ /.
+  This subroutine is internal use to m/ /, s/ / /, split / / and qr/ /.
 
 =item Make character
 
   $chr = Char::Eeucjp::chr($code);
   $chr = Char::Eeucjp::chr_;
 
-  This function returns a programmer-visible character, character represented by
+  This subroutine returns a programmer-visible character, character represented by
   that $code in the character set. For example, Char::Eeucjp::chr(65) is "A" in either
   ASCII or EUC-JP, not Unicode. For the reverse of Char::Eeucjp::chr, use Char::EUCJP::ord.
 
@@ -3613,11 +3613,11 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   @glob = Char::Eeucjp::glob($string);
   @glob = Char::Eeucjp::glob_;
 
-  This function returns the value of $string with filename expansions the way a
+  This subroutine returns the value of $string with filename expansions the way a
   DOS-like shell would expand them, returning the next successive name on each
   call. If $string is omitted, $_ is globbed instead. This is the internal
-  function implementing the <*> and glob operator.
-  This function function when the pathname ends with chr(0x5C) on MSWin32.
+  subroutine implementing the <*> and glob operator.
+  This subroutine function when the pathname ends with chr(0x5C) on MSWin32.
 
   For ease of use, the algorithm matches the DOS-like shell's style of expansion,
   not the UNIX-like shell's. An asterisk ("*") matches any sequence of any
@@ -3630,7 +3630,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   backslashes if you are putting them in literally, due to double-quotish parsing
   of the pattern by perl.
 
-  The Char::Eeucjp::glob function grandfathers the use of whitespace to separate multiple
+  The Char::Eeucjp::glob subroutine grandfathers the use of whitespace to separate multiple
   patterns such as <*.c *.h>. If you want to glob filenames that might contain
   whitespace, you'll have to use extra quotes around the spacy filename to protect
   it. For example, to glob filenames that have an "e" followed by a space followed
